d0b1042604994766a595a71a26ce4da1a184fcae
xKobeni <johnadrian.perce@gmail.com>
2026-03-06 15:43:07 +0800

    removed the frontend file

--- frontend/src/services/notificationsService.js
+++
@@ -1,7 +0,0 @@
-import api from '../config/api';
-import { API_ENDPOINTS } from '../config/constants';
-
-export const getUnreadCountService = async () => {
-  const response = await api.get(API_ENDPOINTS.NOTIFICATIONS.COUNT);
-  return response.data?.unreadCount ?? 0;
-};
